aeec6c749fb97f7115fd9a1260843ac1e631a501
thehood02 <yash28847@gmail.com>
2023-03-15 21:00:36 +0530

    add sample video

--- src/Reels.js
+++ src/Reels.js
@@ -1,4 +1,5 @@
 import {View, TouchableOpacity, Image, StyleSheet} from "react-native";
+import Video from "react-native-video";
 import MyAppText from "./CustomComponents/MyAppText";
 
 const VideoTitle = ({title, time}) => {
@@ -29,15 +30,25 @@ const Reels = () => {
       <View style={styles.mainContainer}>
         <View style={{flexDirection: 'row'}}>
           <VideoTitle title={'video title'} time={'1 day ago'} />
-          <TouchableOpacity
-            activeOpacity={0.8}
-            style={styles.addBtn}>
-            <Image source={require('../assets/icons/add-icon.png')} style={styles.addIcon} />
+          <TouchableOpacity activeOpacity={0.8} style={styles.addBtn}>
+            <Image
+              source={require('../assets/icons/add-icon.png')}
+              style={styles.addIcon}
+            />
             <MyAppText style={{fontFamily: 'Montserrat-SemiBold'}}>
               add video
             </MyAppText>
           </TouchableOpacity>
         </View>
+        <Video
+          paused={false}
+          repeat={true}
+          resizeMode={"cover"}
+          source={{
+            uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+          }}
+          style={{flex: 1, borderRadius: 36, marginTop: 32}}
+        />
       </View>
     );
 }
@@ -46,6 +57,7 @@ export default Reels;
 
 const styles = StyleSheet.create({
   mainContainer: {
+    flex: 1,
     minHeight: '100%',
     backgroundColor: '#000',
     padding: 30,
